5ba6b1fc793630f97a2298dad7fb408391e6ef35
silentFellow <gowtham.sri+github@zohomail.in>
2025-11-24 20:04:52 +0530

    replace husky with a script

--- commit.sh
+++ commit.sh
@@ -5,15 +5,15 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-@echo "Cleaning up generated files..."
+echo "Cleaning up generated files..."
 rm -rf assets/styles/tailwind.css
 rm -rf public
 
-@echo "Running build process..."
+echo "Running build process..."
 npx @tailwindcss/cli -i ./assets/styles/index.css -o ./assets/styles/tailwind.css
 hugo --minify
 
-@echo "Git operations..."
+echo "Git operations..."
 git add .
 git commit -m "$1"
 git push origin main
